3d9ea076ff25784d43313eee69d6efda891d9d0a
Rihemsaadalli2022 <101406064+Rihemsaadalli2022@users.noreply.github.com>
2022-04-23 23:40:33 +0200

    Update fonction.javascript.js

--- fonction javascript/fonction.javascript.js
+++ fonction javascript/fonction.javascript.js
@@ -2,6 +2,6 @@ function factoriel(){
     var n = parseInt(document.form.input.value);
     for(var i = n-1; i >= 1; i=i-1){
         n= n*i;
-        document.form.answer.value=n;
+        document.form.answer.value=n; 111111
     }
 }
